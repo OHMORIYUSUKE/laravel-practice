9e4456dbd632a21d0e515dfe6b80b1d4a11cc91a
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-04-19 17:24:49 +0900

    Fix Log

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Log;
 
 class PostController extends Controller
 {
     // jsonを返すRestAPI
     public function postsAll(){
         // 全部取得
-        $items = DB::select('SELECT * FROM post LEFT JOIN user ON post.user_id = user.id');
+        $items = DB::select('SELECT * FROM post LEFT JOIN user ON post.u_id = user.u_id');
+        Log::debug(print_r($items, true));
         return response()->json(
             $items,
             200,
