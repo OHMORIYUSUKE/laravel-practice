051b30c416b831458f683e3c8aa502d013696ab3
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-04-24 18:05:35 +0900

    Del Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Kreait\Firebase\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Log;
 
 class PostController extends Controller
 {
-    // jsonを返すRestAPI
+    private $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    // 投稿をすべて返す
     public function postsAll(){
         // 全部取得
         $items = DB::select('SELECT * FROM post LEFT JOIN user ON post.u_id = user.u_id');
@@ -21,19 +29,27 @@ class PostController extends Controller
             JSON_UNESCAPED_SLASHES
         );
     }
-    // JSONのポストを実行
+    
+    // 投稿を追加する
     public function insertPost(Request $request){
-        $u_id = $request->input('u_id');
+        $idToken = $request->input('idToken');
         $content = $request->input('content');
+
+        try {
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);   
+        }catch (\Exception $e) {
+            return response()->json(['message' => 'Token error'],500);
+        }
+        $uid = $verifiedIdToken->claims()->get('sub');
 
         DB::insert('INSERT INTO post 
                     (u_id, content) 
                     values (?, ?)', 
-                    [$u_id,$content]
+                    [$uid,$content]
                 );
 
         return response()->json([
-            'u_id' => $u_id,
+            'u_id' => $uid,
             'content' => $content,
          ]);
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Kreait\Firebase\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Log;
@@ -10,24 +11,58 @@ use Log;
 
 class UserController extends Controller
 {
-    // JSONのポストを実行
-    public function insertUser(Request $request){
-        $u_id = $request->input('u_id');
-        $name = $request->input('name');
-        $refreshToken = $request->input('refreshToken');
-        $icon_url = $request->input('icon_url');
+    private $auth;
 
-        DB::insert('INSERT INTO user 
-                    (u_id, name, refreshToken, icon_url) 
-                    values (?, ?, ? ,?)', 
-                    [$u_id, $name, $refreshToken, $icon_url]
-                );
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    // ユーザー追加
+    public function insertUser(Request $request){
+        $idToken = $request->input('idToken');
+        try {
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);   
+        }catch (\Exception $e) {
+            return response()->json(['message' => 'Token error'],500);
+        }
+        $uid = $verifiedIdToken->claims()->get('sub');
+
+        $firebase_user = $this->auth->getUser($uid);
+
+        try {
+            DB::insert('INSERT INTO user 
+                        (u_id, name, icon_url) 
+                        values (?, ? ,?)', 
+                        [$uid, $firebase_user->displayName, $firebase_user->photoUrl]
+                    );
+        }catch (\Exception $e) {
+            return abort(response()->json(['message' => 'user already exists'],500));
+        }
 
         return response()->json([
-            'u_id' => $u_id,
-            'name' => $name,
-            'refreshToken' => $refreshToken,
-            'icon_url' => $icon_url,
-         ]);
+            'u_id' => $uid,
+            'name' => $firebase_user->displayName, 
+            'icon_url' => $firebase_user->photoUrl,
+        ]);
+    }
+    
+    // ユーザー削除
+    public function deleteUser(Request $request){
+        $idToken = $request->input('idToken');
+
+        try {
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);   
+        }catch (\Exception $e) {
+            return response()->json(['message' => 'Token error'],500);
+        }
+        $uid = $verifiedIdToken->claims()->get('sub');
+
+        $this->auth->disableUser($uid);
+
+        return response()->json([
+            'message' => 'user deleted',
+            'u_id' => $uid,
+        ]);
     }
 }
